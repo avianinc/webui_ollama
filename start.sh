b557ebe2b08cc542f6398df9f42bc7a0f38884f5
avianllc <93828723+avianinc@users.noreply.github.com>
2024-06-11 20:04:17 -0400

    still working :)

--- start.sh
+++ start.sh
@@ -11,4 +11,4 @@ docker run -d -p 9099:9099 --add-host=host.docker.internal:host-gateway -e PIPEL
 # docker run -d -p 9099:9099 --add-host=host.docker.internal:host-gateway -v pipelines:/app/pipelines --name pipelines --restart always ghcr.io/open-webui/pipelines:main
 
 # Run the cloudflair host
-docker run -d --network host cloudflare/cloudflared:latest tunnel --no-autoupdate run --token $CLOUDFLARE_TOKEN_INT64_VPC1
+#docker run -d --network host cloudflare/cloudflared:latest tunnel --no-autoupdate run --token $CLOUDFLARE_TOKEN_INT64_VPC1
